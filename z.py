b8aba38d02f7a3e09d7f99074d61671035a7b6b6
Jaedee Sy Korra <jaedesy@outlook.com>
2018-04-02 04:05:33 -0700

    Update z.py

--- z.py
+++ z.py
@@ -81,7 +81,8 @@ def proxy_server(sw, port, f, g, a):
 				dar = "%s KB" % (dar)
 				print("[*] Request Done: %s => %s <=" % (str(a[0]),str(dar)))
 				f.send(c.recv(4096))
-				print(c.recv(4096))
+				f, new = f.accept()
+				print(f.recv(4096))
 				continue
 			else:
 				c.send(c.recv(4096))
