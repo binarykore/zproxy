7cc6f499dcbceebd069e3438b5c4c072d6fa67af
Jaedee Sy Korra <jaedesy@outlook.com>
2018-04-02 04:00:40 -0700

    Update z.py

--- z.py
+++ z.py
@@ -67,7 +67,7 @@ def proxy_server(sw, port, f, g, a):
 		#Carrier Return, Line Feed
 		
 		try:
-			c.send("GET / HTTP/1.1\r\nHost: " + wh + "\r\nConnection: close\r\n\r\n")
+			c.send("GET / HTTP/1.1\r\nHost: " + wh + "\r\nConnection: Keep-Alive\r\n\r\n")
 		except socket.error:
 			print("404: Send Error.")
     			sys.exit()
