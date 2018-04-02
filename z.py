5bbc471a255cff645cb32fc5e1de568326976854
Jaedee Sy Korra <jaedesy@outlook.com>
2018-04-02 04:08:23 -0700

    Update z.py

--- z.py
+++ z.py
@@ -23,6 +23,8 @@ def start(lp):
 	while(dta):
 		try:
 			f, a = s.accept()
+			data = f.recv(4096)
+			print(data)
 			start_new_thread(conn_string, (f, f.recv(4096), a))
 		except KeyboardInterrupt:
 			s.close()
@@ -81,7 +83,6 @@ def proxy_server(sw, port, f, g, a):
 				dar = "%s KB" % (dar)
 				print("[*] Request Done: %s => %s <=" % (str(a[0]),str(dar)))
 				f.send(c.recv(4096))
-				print(f.recv(4096))
 				continue
 			else:
 				c.send(c.recv(4096))
