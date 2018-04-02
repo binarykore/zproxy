fc2e6b3e77fbfd636b5f406702eae3b1698f1e50
Jaedee Sy Korra <jaedesy@outlook.com>
2018-04-02 04:17:27 -0700

    Update z.py

--- z.py
+++ z.py
@@ -31,12 +31,12 @@ def start(lp):
 			sys.exit(1)
 	s.close()
 def conn_string(f, g, a, data):
-	print(data)
-	if(data != null):
+	if(data != ""):
 		print("[MINED]: " + data)
 		minero = data
 	else:
-		print("[ABANDONED]: Mine")
+		print("[ABANDONED]: Mine.")
+		minero = 0
 	try:
 		first_line = g.split("\n")[0]
 
