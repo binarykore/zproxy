e1f86426ba7efb822a8155afc98d597c8f2127bb
Jaedee Sy Korra <jaedesy@outlook.com>
2018-04-02 03:56:34 -0700

    Update z.py

--- z.py
+++ z.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
-import socket, sys, threading, re, ssl
+import socket, sys, re, ssl
 from sys import *
 from socket import *
-from threading import *
+from thread import *
 from ssl import *
 from re import *
 
